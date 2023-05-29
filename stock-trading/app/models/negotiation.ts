dc8c6baa198196d2e353c8c01938e160341e722e
renarasecchim <renarasecchim@gmail.com>
2023-05-29 15:58:17 -0300

    chore: add mod typscript and sctruture types

---
+++ stock-trading/app/models/negotiation.ts
@@ -0,0 +1,27 @@
+export class Negotiation {
+	private _date : Date
+	private _amount: number
+	private _value: number
+
+	constructor(date: Date, amount: number, value: number){
+		this._date = date;
+		this._amount = amount;
+		this._value = value;
+	}
+
+	get date() {
+		return this._date;
+	}
+
+	get amount() {
+		return this._amount;
+	}
+
+	get value() {
+		return this._value;
+	}
+
+	get volume() {
+		return this._amount * this._value;
+	}
+}
